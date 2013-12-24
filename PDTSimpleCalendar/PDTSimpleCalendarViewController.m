484343726ded9afb38e545b1daa4b261f4f9c583
Jeroen Houtzager <houtzager@gmail.com>
2013-12-24 12:38:50 +0100

    Allow for different day numbering in other locales (Arabic etc)

--- PDTSimpleCalendar/PDTSimpleCalendarViewController.m
+++ PDTSimpleCalendar/PDTSimpleCalendarViewController.m
@@ -24,6 +24,7 @@ static NSString *PDTSimpleCalendarViewHeaderIdentifier = @"com.producteev.collec
 
 @property (nonatomic, strong) UILabel *overlayView;
 @property (nonatomic, strong) NSDateFormatter *headerDateFormatter; //Will be used to format date in header view and on scroll.
+@property (nonatomic, strong) NSDateFormatter *dayDateFormatter; //Will be used to format days in month view.
 
 @end
 
@@ -85,6 +86,16 @@ static NSString *PDTSimpleCalendarViewHeaderIdentifier = @"com.producteev.collec
         _headerDateFormatter.dateFormat = [NSDateFormatter dateFormatFromTemplate:@"yyyyLLLL" options:0 locale:[NSLocale currentLocale]];
     }
     return _headerDateFormatter;
+}
+
+- (NSDateFormatter *)dayDateFormatter;
+{
+    if (!_dayDateFormatter) {
+        _dayDateFormatter = [[NSDateFormatter alloc] init];
+        _dayDateFormatter.calendar = self.calendar;
+        _dayDateFormatter.dateFormat = [NSDateFormatter dateFormatFromTemplate:@"d" options:0 locale:[NSLocale currentLocale]];
+    }
+    return _dayDateFormatter;
 }
 
 - (NSCalendar *)calendar
@@ -198,7 +209,7 @@ static NSString *PDTSimpleCalendarViewHeaderIdentifier = @"com.producteev.collec
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+    // Do any additional setup after loading the view.
 
     //Configure the Collection View
     [self.collectionView registerClass:[PDTSimpleCalendarViewCell class] forCellWithReuseIdentifier:PDTSimpleCalendarViewCellIdentifier];
@@ -258,7 +269,7 @@ static NSString *PDTSimpleCalendarViewHeaderIdentifier = @"com.producteev.collec
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
 {
     PDTSimpleCalendarViewCell *cell = [self.collectionView dequeueReusableCellWithReuseIdentifier:PDTSimpleCalendarViewCellIdentifier
-                                                                           forIndexPath:indexPath];
+                                                                                     forIndexPath:indexPath];
 
     NSDate *firstOfMonth = [self firstOfMonthForSection:indexPath.section];
     NSDate *cellDate = [self dateForCellAtIndexPath:indexPath];
@@ -271,7 +282,7 @@ static NSString *PDTSimpleCalendarViewHeaderIdentifier = @"com.producteev.collec
     BOOL isSelected = NO;
 
     if (cellDateComponents.month == firstOfMonthsComponents.month) {
-        cellTitleString = [NSString stringWithFormat:@"%@", @(cellDateComponents.day)];
+        cellTitleString = [self.dayDateFormatter stringFromDate:cellDate];
         isSelected = ([self isSelectedDate:cellDate] && (indexPath.section == [self sectionForDate:cellDate]));
         isToday = [self isTodayDate:cellDate];
     }
@@ -322,7 +333,7 @@ static NSString *PDTSimpleCalendarViewHeaderIdentifier = @"com.producteev.collec
 
         headerView.layer.shouldRasterize = YES;
         headerView.layer.rasterizationScale = [UIScreen mainScreen].scale;
-        
+
         return headerView;
     }
 
@@ -440,15 +451,15 @@ static NSString *PDTSimpleCalendarViewHeaderIdentifier = @"com.producteev.collec
     }
 
     NSInteger section = [self sectionForDate:date];
-
+    
     NSDate *firstOfMonth = [self firstOfMonthForSection:section];
     NSInteger ordinalityOfFirstDay = [self.calendar ordinalityOfUnit:NSDayCalendarUnit inUnit:NSWeekCalendarUnit forDate:firstOfMonth];
-
-
+    
+    
     NSDateComponents *dateComponents = [self.calendar components:NSDayCalendarUnit fromDate:date];
     NSDateComponents *firstOfMonthComponents = [self.calendar components:NSDayCalendarUnit fromDate:firstOfMonth];
     NSInteger item = (dateComponents.day - firstOfMonthComponents.day) - (1 - ordinalityOfFirstDay);
-
+    
     return [NSIndexPath indexPathForItem:item inSection:section];
 }
 
