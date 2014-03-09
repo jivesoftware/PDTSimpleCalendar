824ae51101ad17685714eb7b5abad9b146075d3b
Jeroen Houtzager <houtzager@gmail.com>
2014-03-09 13:37:11 +0100

    Easify use of different calendars in demo app delegate

--- PDTSimpleCalendarDemo/PDTSimpleCalendarDemo/PDTAppDelegate.m
+++ PDTSimpleCalendarDemo/PDTSimpleCalendarDemo/PDTAppDelegate.m
@@ -32,9 +32,27 @@
     PDTSimpleCalendarViewController *calendarViewController = [[PDTSimpleCalendarViewController alloc] init];
     [calendarViewController setDelegate:self];
     //Example of how you can change the default calendar
-//    NSCalendar *hebrewCalendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSHebrewCalendar];
-//    hebrewCalendar.locale = [NSLocale currentLocale];
-//    [calendarViewController setCalendar:hebrewCalendar];
+
+    //#define TRY_ARAB
+    //#define TRY_HEBREW
+    //#define TRY_FARSI
+    //#define TRY_INDIAN
+
+
+#if defined(TRY_HEBREW)
+    NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSHebrewCalendar];
+#elif defined(TRY_FARSI)
+    NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSPersianCalendar];
+#elif defined(TRY_ARAB)
+    NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSIslamicCalendar];
+#elif defined(TRY_INDIAN)
+    NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSIndianCalendar];
+#else
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+#endif
+    [calendarViewController setCalendar:calendar];
+    NSLog(@"Calendar ID = %@", calendar.calendarIdentifier);
+    NSLog(@"Language = %@", [[NSLocale preferredLanguages] firstObject]);
 
     UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:calendarViewController];
     [calendarViewController setTitle:@"SimpleCalendar"];
