e26e8a318ca541d21a17ae9f58f89ddca790aba8
Jerome Miglino <jerome.miglino@gmail.com>
2014-11-04 11:39:24 -0500

    Update pbxproj & migrate to XCTest

--- PDTSimpleCalendarDemo/PDTSimpleCalendarDemoTests/PDTSimpleCalendarDemoTests.m
+++ PDTSimpleCalendarDemo/PDTSimpleCalendarDemoTests/PDTSimpleCalendarDemoTests.m
@@ -6,11 +6,11 @@
 //  Copyright (c) 2014 Producteev. All rights reserved.
 //
 
-#import <SenTestingKit/SenTestingKit.h>
+#import <XCTest/XCTest.h>
 #import "PDTSimpleCalendarViewController.h"
 #import "PDTSimpleCalendarViewController+Private.h"
 
-@interface PDTSimpleCalendarDemoTests : SenTestCase
+@interface PDTSimpleCalendarDemoTests : XCTestCase
 
 @property (nonatomic, strong) PDTSimpleCalendarViewController *calendarController;
 
@@ -40,16 +40,16 @@
     components.day = 1;
     NSDate *expectedFirstDate = [currentCalendar dateFromComponents:components];
 
-    STAssertTrue([_calendarController.firstDate isEqualToDate:expectedFirstDate], @"firstDate should be equal to the first day of the month");
-    STAssertTrue([_calendarController.firstDateMonth isEqualToDate:expectedFirstDate], @"firstDateMonth should be equal to the first day of the month");
+    XCTAssertTrue([_calendarController.firstDate isEqualToDate:expectedFirstDate], @"firstDate should be equal to the first day of the month");
+    XCTAssertTrue([_calendarController.firstDateMonth isEqualToDate:expectedFirstDate], @"firstDateMonth should be equal to the first day of the month");
 
     NSDateComponents *addOneYearComponents = [[NSDateComponents alloc] init];
     addOneYearComponents.year =1;
     addOneYearComponents.day = -1;
     NSDate *expectedLastDate = [currentCalendar dateByAddingComponents:addOneYearComponents toDate:expectedFirstDate options:0];
 
-    STAssertTrue([_calendarController.lastDate isEqualToDate:expectedLastDate], @"lastDate should be equal to the last day of the month in one year");
-    STAssertTrue([_calendarController.lastDateMonth isEqualToDate:expectedLastDate], @"lastDateMonth should be equal to the last day of the month in one year");
+    XCTAssertTrue([_calendarController.lastDate isEqualToDate:expectedLastDate], @"lastDate should be equal to the last day of the month in one year");
+    XCTAssertTrue([_calendarController.lastDateMonth isEqualToDate:expectedLastDate], @"lastDateMonth should be equal to the last day of the month in one year");
 }
 
 - (void)testSetFirstDate
@@ -64,13 +64,13 @@
     components.day = 1;
     NSDate *expectedFirstDate = [currentCalendar dateFromComponents:components];
 
-    STAssertTrue([_calendarController.firstDate isEqualToDate:clampedToday], @"firstDate should be today");
-    STAssertTrue([_calendarController.firstDateMonth isEqualToDate:expectedFirstDate], @"firstDateMonth should be equal to the first day of the month");
+    XCTAssertTrue([_calendarController.firstDate isEqualToDate:clampedToday], @"firstDate should be today");
+    XCTAssertTrue([_calendarController.firstDateMonth isEqualToDate:expectedFirstDate], @"firstDateMonth should be equal to the first day of the month");
     //Just to be sure
     NSDateComponents *firstDateMonthComponents = [currentCalendar components:NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay fromDate:_calendarController.firstDateMonth];
     NSDateComponents *firstDateComponents = [currentCalendar components:NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay fromDate:_calendarController.firstDate];
-    STAssertTrue(firstDateComponents.month == firstDateMonthComponents.month, @"FirstDate & firstDateMonth should be in the same month");
-    STAssertTrue(firstDateComponents.day >= firstDateMonthComponents.day, @"firstDate day should be >= firstDateMonth");
+    XCTAssertTrue(firstDateComponents.month == firstDateMonthComponents.month, @"FirstDate & firstDateMonth should be in the same month");
+    XCTAssertTrue(firstDateComponents.day >= firstDateMonthComponents.day, @"firstDate day should be >= firstDateMonth");
 }
 
 - (void)testSetLastDate
@@ -86,7 +86,7 @@
     NSDate *lastDate = [currentCalendar dateByAddingComponents:addComponents toDate:clampedToday options:0];
     _calendarController.lastDate = lastDate;
 
-    STAssertTrue([_calendarController.lastDate isEqualToDate:lastDate], @"lastDate should be in 3 months from today");
+    XCTAssertTrue([_calendarController.lastDate isEqualToDate:lastDate], @"lastDate should be in 3 months from today");
 
     //Calculate Last Day of Month
     components = [currentCalendar components:NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay fromDate:lastDate];
@@ -95,12 +95,12 @@
     //Next month with day zero, gives the last day of month.
     NSDate *expectedLastDayMonthDate = [currentCalendar dateFromComponents:components];
 
-    STAssertTrue([_calendarController.lastDateMonth isEqualToDate:expectedLastDayMonthDate], @"firstDateMonth should be equal to the first day of the month");
+    XCTAssertTrue([_calendarController.lastDateMonth isEqualToDate:expectedLastDayMonthDate], @"firstDateMonth should be equal to the first day of the month");
     //Just to be sure
     NSDateComponents *lastDateMonthComponents = [currentCalendar components:NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay fromDate:_calendarController.lastDateMonth];
     NSDateComponents *lastDateComponents = [currentCalendar components:NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay fromDate:_calendarController.lastDate];
-    STAssertTrue(lastDateComponents.month == lastDateMonthComponents.month, @"LastDate & lastDateMonth should be in the same month");
-    STAssertTrue(lastDateComponents.day <= lastDateMonthComponents.day, @"LastDate day should be < lastDateMonth");
+    XCTAssertTrue(lastDateComponents.month == lastDateMonthComponents.month, @"LastDate & lastDateMonth should be in the same month");
+    XCTAssertTrue(lastDateComponents.day <= lastDateMonthComponents.day, @"LastDate day should be < lastDateMonth");
 }
 
 - (void)testNumberOfDaysPerWeek
@@ -110,7 +110,7 @@
     //We know that the gregorian calendar has 7 days per week
     //If you know a calendar who doesn't have 7 days per week and is supported by iOS let me know ;-)
 
-    STAssertTrue(_calendarController.daysPerWeek == 7, @"Should be 7 days a week");
+    XCTAssertTrue(_calendarController.daysPerWeek == 7, @"Should be 7 days a week");
 }
 
 @end
