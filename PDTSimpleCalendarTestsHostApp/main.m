99775fa0794640f8de4bc3b55f31e8c15dd69ac9
Jerome Miglino <jerome.miglino@gmail.com>
2015-08-07 18:49:44 -0400

    Add Host App for Tests & Update Travis script

---
+++ PDTSimpleCalendarTestsHostApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PDTSimpleCalendarTestsHostApp
+//
+//  Created by Jerome Miglino on 8/7/15.
+//  Copyright (c) 2015 Jive Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
